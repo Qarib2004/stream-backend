0983a36108a7991edb07983419059a7e0cdaa14f
Qarib2004 <garibalisultanov@gmail.com>
2025-05-27 15:17:29 +0400

    complected back functions

---
+++ src/core/prisma/prisma.service.ts
@@ -0,0 +1,14 @@
+import { INestApplication, Injectable,type OnModuleDestroy,type OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@/generated/prisma'; 
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit,OnModuleDestroy {
+  public async onModuleInit() {
+    await this.$connect();
+  }
+
+  public async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+}
